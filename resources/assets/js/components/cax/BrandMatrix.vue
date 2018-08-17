3a4d0e2f080560f92fba356d6cb57ceaf6c9752a
HiKangg <kangg1987@hotmail.com>
2018-08-16 23:23:30 -0700

    display component log.

--- resources/assets/js/components/cax/BrandMatrix.vue
+++ resources/assets/js/components/cax/BrandMatrix.vue
@@ -72,7 +72,7 @@
          */
         mounted() {
             this.prepareComponent();
-//            console.log('Component mounted. v1.1');
+            console.log('Component mounted. v1.1');
             this.choices = this.currentSelection;
 
         },
