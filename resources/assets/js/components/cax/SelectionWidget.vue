42117d3f02cba2c8b0401ac0854195684928d2d2
HiKangg <kangg1987@hotmail.com>
2018-08-27 08:36:31 -0700

    add handle selection change.

--- resources/assets/js/components/cax/SelectionWidget.vue
+++ resources/assets/js/components/cax/SelectionWidget.vue
@@ -45,7 +45,7 @@
 //                this.getBrands();
             },
 
-/*
+
             handleSelectionChange(collectedIds) {
                 for (let value of collectedIds) {
                     value += 1;
@@ -53,10 +53,6 @@
                 }
                 alert('Selection changed!' + collectedIds)
             }
-*/
-
-
-
         }
 
     }
