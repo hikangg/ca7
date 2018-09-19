e818c9b0c72fb16a55f5bbd1fdfeee085035415f
HiKangg <kangg1987@hotmail.com>
2018-09-18 23:32:48 -0700

    display console.log for BrandsNavBar.vue

--- resources/assets/js/components/cax/BrandsNavBar.vue
+++ resources/assets/js/components/cax/BrandsNavBar.vue
@@ -135,7 +135,7 @@
                     brandsviewmode: mode,
                     sender: 'brands-view-mode-selector'
                 });
-//                console.log('Broadcast from BrandsViewModeSelector: "' + myEvent + '", changed view mode to: ' + mode);
+                console.log('Broadcast from BrandsViewModeSelector: "' + myEvent + '", changed view mode to: ' + mode);
             },
 
             /**
@@ -147,7 +147,7 @@
                     basketName: newName,
                     sender: 'brands-nav-bar'
                 });
-//                console.log('Broadcast from BrandsNavBar: "' + myEvent + '", save as new basket: ' + newName);
+                console.log('Broadcast from BrandsNavBar: "' + myEvent + '", save as new basket: ' + newName);
             },
 
             /**
@@ -163,9 +163,9 @@
                         brandsviewmode: mode,
                         sender: 'brands-view-mode-selector'
                     });
-//                    console.log('Broadcast from BrandsViewModeSelector: "' + myEvent + '", changed view mode to: ' + mode);
+                    console.log('Broadcast from BrandsViewModeSelector: "' + myEvent + '", changed view mode to: ' + mode);
 
-//                    console.log('new viewMode is ' + this.viewMode);
+                    console.log('new viewMode is ' + this.viewMode);
 
                 }
             },
@@ -174,12 +174,12 @@
                 axios.post('/api/baskets', {name: val})
                     .then(response => {
                         this.log = 'posted' + val;
-//                        console.log('posted "' + val + '"');
-//                        console.log(response.status);
+                        console.log('posted "' + val + '"');
+                        console.log(response.status);
                     })
                     .catch(error => {
                         this.errors = error;
-//                        console.log(error);
+                        console.log(error);
                     });
             },
 
@@ -190,9 +190,9 @@
              */
             requestBasketSave(val) {
                 if (val) {
-//                    console.log('Save with new name ' + val);
+                    console.log('Save with new name ' + val);
                 } else {
-//                    console.log('Saving basket');
+                    console.log('Saving basket');
                 }
                 this.saveBasket(val);
                 this.broadcastBasketNeedsSaving(val);
